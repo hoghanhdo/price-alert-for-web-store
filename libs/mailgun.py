07db37cf3e6e59fcf1be5512983fbac298e1545a
hoghanhdo@gmail.com <45198696+hoghanhdo@users.noreply.github.com>
2020-11-04 00:51:14 +0900

    changes on constants

--- libs/mailgun.py
+++ libs/mailgun.py
@@ -10,12 +10,12 @@ class MailgunException(Exception):
 
 class Mailgun:
     FROM_TITLE = "Pricing Alert Service"
-    FROM_EMAIL = "do-not-reply@sandboxea7f94db2e4c4b58ba2c8920609d4c4b.mailgun.org"
 
     @classmethod
     def send_mail(cls, email: List[str], subject: str, text: str, html: str) -> Response:
         api_key = os.environ.get("MAILGUN_API_KEY", None)
         domain = os.environ.get("MAILGUN_DOMAIN", None)
+        from_email = os.environ.get("FROM_EMAIL", None)
         if api_key is None:
             raise MailgunException("Failed to get Mailgun API Key")
         if domain is None:
@@ -23,7 +23,7 @@ class Mailgun:
         response = post(
             f"{domain}/messages",
             auth=("api", f"{api_key}"),
-            data={"from": f"{cls.FROM_TITLE} <{cls.FROM_EMAIL}>",
+            data={"from": f"{cls.FROM_TITLE} <{from_email}>",
                   "to": email,
                   "subject": subject,
                   "text": text,
